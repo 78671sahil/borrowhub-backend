721475e257388ed69b9aa308a83ee647bedaea50
78671sahil <sahilbuhana9024@gmail.com>
2026-02-23 18:27:12 +0530

    k

--- src/utils/otp.util.js
+++ src/utils/otp.util.js
@@ -1,4 +1,9 @@
  export const generateOTP = () => {
   return Math.floor(100000 + Math.random() * 900000).toString();
 };
- 
+
+// 🔥 FIX: Ye export missing tha isliye SyntaxError aa raha tha
+export const sendSMS = async (phone, otp) => {
+  console.log(`[OTP LOG] Phone: ${phone}, OTP: ${otp}`);
+  // Future mein yahan WhatsApp ya Fast2SMS ka logic daal sakte ho
+};
