d4d52ec37739250c36bbc004f849d48c76081822
78671sahil <sahilbuhana9024@gmail.com>
2026-02-23 17:35:58 +0530

    backend-1

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
  import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import  User  from "../models/user.model.js";
- import { generateOTP, sendSMS } from "../utils/otp.util.js";
+ import { generateOTP } from "../utils/otp.util.js";
 
  
 
